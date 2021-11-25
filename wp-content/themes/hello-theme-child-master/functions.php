9b5804622abdf0e2dfbeefb021f6fbae50501830
Michael Rehnert (killshot13) <dmreh@outlook.com>
2021-11-25 08:26:30 -0600

    Style(sthprod) added funtion to remove 'archive' from page titles

--- wp-content/themes/hello-theme-child-master/functions.php
+++ wp-content/themes/hello-theme-child-master/functions.php
@@ -39,6 +39,18 @@ function ele_disable_page_title($return)
 }
 add_filter('hello_elementor_page_title', 'ele_disable_page_title');
 
+add_filter('get_the_archive_title', function ($title) {
+    if (is_category()) {
+        $title = single_cat_title('', false);
+    } elseif (is_tag()) {
+        $title = single_tag_title('', false);
+    } elseif (is_author()) {
+        $title = '<span class="vcard">'.get_the_author().'</span>';
+    }
+
+    return $title;
+});
+
 function archive_callback($title)
 {
     $str = 'Here\'s What We Found About: ';
